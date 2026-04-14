87a93041be3cc1429e55c6cb42cf59d80dc503bf
ismael-valdivia-D <isma49345@gmail.com>
2026-04-14 16:40:56 -0400

    ola

---
+++ python.py
@@ -0,0 +1,16 @@
+
+print("1.-cereal 2000")
+print("2.-lechuga 1500")
+print("3.- leche 1000")
+print("seleccione su producto")
+op=int(input())
+if op == 1 :
+    print("el valor a pagar es ",2000*1.19)
+else:
+    if op==2 :
+        print("el valor a pagar es ",1500*1.19)
+    else:
+        if op==3:
+            print("el valor a pagar es ",1000*1.19)
+        else:
+            print("Opcion Invalida")
